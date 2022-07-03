faa3841208532c64e684ecd1a12c32b671c34b74
MAlahmed98 <mustafa.alahmed988@gmail.com>
2022-07-03 14:13:16 +0300

    notes are now saved

--- note/views.py
+++ note/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import note
 # Create your views here.
 
@@ -6,12 +6,36 @@ from .models import note
 def editor(request):
     noteid = int(request.GET.get('noteid', 0))
     notes = note.objects.all()
+    
+    NID = 0
+    #in here it's telling me if we requested a form, we do this shit I don't fucking understand 
+    if request.method == 'POST':
+        NID = int(request.POST.get('noteid', 0)) #in here, he gets the ID of the not, and some other shit
+        tit = request.POST.get('title')
+        con = request.POST.get('content', '')
+
+        if NID > 0: #if the note ID is not 0
+            nott = note.objects.get(pk=NID)
+            nott.title = tit
+            nott.content = con
+            nott.save() #this will insert it into the data base
+
+            return redirect('/?noteid=%i' % NID) 
+        else:
+            nott = note.objects.create(title=tit, content=con)
+            
+            return redirect('/?noteid=%i' % nott.id)
+    if NID > 0:
+        nott = note.objects.get(pk=nott)
+    else:
+        nott = ''
 
     content = {
         'noteid': noteid,
-        'notes' : notes
+        'notes' : notes,
+        'note': note
     }
-
+    
     return render(request, 'editor.html', content)
 
 
@@ -23,6 +47,7 @@ def browse(request):
         'noteid': noteid,
         'notes' : notes
     }
+    
     
     return render(request, 'browse.html', content)
 
